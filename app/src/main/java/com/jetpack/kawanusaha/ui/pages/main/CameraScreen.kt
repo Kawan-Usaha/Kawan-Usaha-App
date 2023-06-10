515c0675af3aa9d785c754655d2dac02209bd3fc
Windiarta <windiarta@ui.ac.id>
2023-06-10 20:20:28 +0700

    image getter done

--- app/src/main/java/com/jetpack/kawanusaha/ui/pages/main/CameraScreen.kt
+++ app/src/main/java/com/jetpack/kawanusaha/ui/pages/main/CameraScreen.kt
@@ -4,46 +4,204 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.provider.Settings
 import android.util.Log
 import android.view.ViewGroup
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.*
+import androidx.camera.core.ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BrowseGallery
+import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material.icons.filled.FlipCameraAndroid
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material.AlertDialog
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
+import coil.compose.rememberImagePainter
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionRequired
 import com.google.accompanist.permissions.rememberPermissionState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
 import java.util.concurrent.Executor
-import kotlin.coroutines.suspendCoroutine
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
-@OptIn(ExperimentalPermissionsApi::class)
+val EMPTY_IMAGE_URI: Uri = Uri.parse("file://dev/null")
+
 @Composable
-fun CameraScreen(
+fun CameraScreen(modifier: Modifier = Modifier) {
+    val emptyImageUri = Uri.parse("file://dev/null")
+    var imageUri by remember { mutableStateOf(emptyImageUri) }
+    if (imageUri != emptyImageUri) {
+        Box(modifier = modifier) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = rememberImagePainter(imageUri),
+                contentDescription = "Captured image"
+            )
+            Button(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                onClick = {
+                    imageUri = emptyImageUri
+                }
+            ) {
+                Text("Remove image")
+            }
+        }
+    } else {
+        var showGallerySelect by remember { mutableStateOf(false) }
+        if (showGallerySelect) {
+            GallerySelect(
+                modifier = modifier,
+                onImageUri = { uri ->
+                    showGallerySelect = false
+                    imageUri = uri
+                }
+            )
+        } else {
+            Box(modifier = modifier) {
+                CameraCapture(
+                    modifier = modifier,
+                    onImageFile = { file ->
+                        imageUri = file.toUri()
+                    }
+                )
+                IconButton(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(4.dp),
+                    onClick = {
+                        showGallerySelect = true
+                    },
+                    content = {
+                        Icon(Icons.Default.BrowseGallery, contentDescription = "Gallery")
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CameraCapture(
+    modifier: Modifier = Modifier,
+    onImageFile: (File) -> Unit = { }
+) {
+    var cameraSelector by remember { mutableStateOf(CameraSelector.DEFAULT_BACK_CAMERA) }
+    val context = LocalContext.current
+    Permission(
+        permission = Manifest.permission.CAMERA,
+        rationale = "You said you wanted a picture, so I'm going to have to ask for permission.",
+        permissionNotAvailableContent = {
+            Box(modifier.fillMaxSize()) {
+                Column (horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier.align(Alignment.Center)){
+                    Text("No Permission Camera Granted!")
+                    Spacer(modifier = modifier.height(15.dp))
+                    Button(
+                        onClick = {
+                            context.startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                data = Uri.fromParts("package", context.packageName, null)
+                            })
+                        }) {
+                        Text("Open Settings")
+                    }
+                }
+            }
+        }
+    ) {
+        Box(modifier = modifier) {
+            val lifecycleOwner = LocalLifecycleOwner.current
+            val coroutineScope = rememberCoroutineScope()
+            var previewUseCase by remember { mutableStateOf<UseCase>(Preview.Builder().build()) }
+            val imageCaptureUseCase by remember {
+                mutableStateOf(
+                    ImageCapture.Builder()
+                        .setCaptureMode(CAPTURE_MODE_MAXIMIZE_QUALITY)
+                        .build()
+                )
+            }
+            Box {
+                CameraPreview(
+                    modifier = Modifier.fillMaxSize(),
+                    onUseCase = {
+                        previewUseCase = it
+                    }
+                )
+                IconButton(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(16.dp)
+                        .align(Alignment.BottomCenter),
+                    onClick = {
+                        coroutineScope.launch {
+                            onImageFile(imageCaptureUseCase.takePicture(context.executor))
+                        }
+                    },
+                    content = {
+                        Icon(Icons.Default.Camera, contentDescription = "Take Picture")
+                    }
+                )
+                IconButton(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(16.dp)
+                        .align(Alignment.TopEnd),
+                    onClick = {
+                        cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_FRONT_CAMERA){
+                            CameraSelector.DEFAULT_BACK_CAMERA
+                        } else {
+                            CameraSelector.DEFAULT_FRONT_CAMERA
+                        }
+                    }, content = {
+                        Icon(Icons.Default.FlipCameraAndroid, contentDescription = "Flip Camera")
+                    }
+                )
+            }
+            LaunchedEffect(previewUseCase, cameraSelector) {
+                val cameraProvider = context.getCameraProvider()
+                try {
+                    // Must unbind the use-cases before rebinding them.
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner, cameraSelector, previewUseCase, imageCaptureUseCase
+                    )
+                } catch (ex: Exception) {
+                    Log.e("CameraCapture", "Failed to bind camera use cases", ex)
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CameraPreview(
     modifier: Modifier = Modifier,
     scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
-    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    onUseCase: (UseCase) -> Unit = { }
 ) {
-    val coroutineScope = rememberCoroutineScope()
-    val lifecycleOwner = LocalLifecycleOwner.current
-    MainContent(modifier = Modifier.fillMaxSize())
     AndroidView(
         modifier = modifier,
         factory = { context ->
@@ -54,30 +212,40 @@ fun CameraScreen(
                     ViewGroup.LayoutParams.MATCH_PARENT
                 )
             }
-
-            // CameraX Preview UseCase
-            val previewUseCase = Preview.Builder()
+            onUseCase(Preview.Builder()
                 .build()
                 .also {
                     it.setSurfaceProvider(previewView.surfaceProvider)
                 }
-
-            coroutineScope.launch {
-                val cameraProvider = context.getCameraProvider()
-                try {
-                    // Must unbind the use-cases before rebinding them.
-                    cameraProvider.unbindAll()
-                    cameraProvider.bindToLifecycle(
-                        lifecycleOwner, cameraSelector, previewUseCase
-                    )
-                } catch (ex: Exception) {
-                    Log.e("CameraPreview", "Use case binding failed", ex)
-                }
-            }
-
+            )
             previewView
         }
     )
+}
+
+suspend fun ImageCapture.takePicture(executor: Executor): File {
+    val photoFile = withContext(Dispatchers.IO) {
+        kotlin.runCatching {
+            File.createTempFile("image", "jpg")
+        }.getOrElse { ex ->
+            Log.e("TakePicture", "Failed to create temporary file", ex)
+            File("/dev/null")
+        }
+    }
+
+    return suspendCoroutine { continuation ->
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        takePicture(outputOptions, executor, object : ImageCapture.OnImageSavedCallback {
+            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                continuation.resume(photoFile)
+            }
+
+            override fun onError(ex: ImageCaptureException) {
+                Log.e("TakePicture", "Image capture failed", ex)
+                continuation.resumeWithException(ex)
+            }
+        })
+    }
 }
 
 suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
@@ -134,31 +302,61 @@ private fun Rationale(
     )
 }
 
-@ExperimentalPermissionsApi
 @Composable
-fun MainContent(modifier: Modifier = Modifier) {
+fun GallerySelect(
+    modifier: Modifier = Modifier,
+    onImageUri: (Uri) -> Unit = { }
+) {
     val context = LocalContext.current
-    Permission(
-        permission = Manifest.permission.CAMERA,
-        rationale = "You said you wanted a picture, so I'm going to have to ask for permission.",
-        permissionNotAvailableContent = {
-            Column(modifier) {
-                Text("O noes! No Camera!")
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(
-                    onClick = {
-                        context.startActivity(
-                            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                                data = Uri.fromParts("package", context.packageName, null)
-                            }
-                        )
-                    }
-                ) {
-                    Text("Open Settings")
-                }
-            }
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri: Uri? ->
+            onImageUri(uri ?: EMPTY_IMAGE_URI)
         }
-    ) {
-        Text("It worked!")
+    )
+
+    @Composable
+    fun LaunchGallery() {
+        SideEffect {
+            launcher.launch("image/*")
+        }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        Permission(
+            permission = Manifest.permission.ACCESS_MEDIA_LOCATION,
+            rationale = "You want to read from photo gallery, so I'm going to have to ask for permission.",
+            permissionNotAvailableContent = {
+                Column(modifier) {
+                    Text("O noes! No Photo Gallery!")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        Button(
+                            modifier = Modifier.padding(4.dp),
+                            onClick = {
+                                context.startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                    data = Uri.fromParts("package", context.packageName, null)
+                                })
+                            }
+                        ) {
+                            Text("Open Settings")
+                        }
+                        // If they don't want to grant permissions, this button will result in going back
+                        Button(
+                            modifier = Modifier.padding(4.dp),
+                            onClick = {
+                                onImageUri(EMPTY_IMAGE_URI)
+                            }
+                        ) {
+                            Text("Use Camera")
+                        }
+                    }
+                }
+            },
+        ) {
+            LaunchGallery()
+        }
+    } else {
+        LaunchGallery()
     }
 }
